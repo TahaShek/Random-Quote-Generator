f92e3e1098e24781bcc5948c1c2574ad9635959c
TahaShek <sohailtaha128@gmail.com>
2022-09-07 04:35:29 +0500

    commti

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
-let qoute=document.querySelector('.qoute')
-let person=document.querySelector('.person')
-let btn=document.querySelector('.btn')
+// let qoute=document.querySelector('.qoute')
+// let person=document.querySelector('.person')
+// let btn=document.querySelector('.btn')
 
-let qoutes=[
-    {Qoute:`Be who you are and say what you feel, because those who mind dont matter, and those who matter dont mind.`,
-    Person:" Bernard M. Baruch"},
+// let qoutes=[
+//     {Qoute:`Be who you are and say what you feel, because those who mind dont matter, and those who matter dont mind.`,
+//     Person:" Bernard M. Baruch"},
     
-    {Qoute:`Life is what happens to us while we are making other plans.`,
-    Person:" Jahaz"},
+//     {Qoute:`Life is what happens to us while we are making other plans.`,
+//     Person:" Jahaz"},
 
-    {Qoute:`In three words I can sum up everything I've learned about life: it goes on.`,
-    Person:"  Robert Frost"},
+//     {Qoute:`In three words I can sum up everything I've learned about life: it goes on.`,
+//     Person:"  Robert Frost"},
 
-    {Qoute:`Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.`,
-    Person:" Marilyn Monroe"}
+//     {Qoute:`Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.`,
+//     Person:" Marilyn Monroe"}
 
-]
-btn.addEventListener('click',function(){
-    let random=Math.floor(Math.random()*qoutes.length)
-    qoute.innerText= qoutes[random].Qoute
-    person.innerText= qoutes[random].Person
+// ]
+// btn.addEventListener('click',function(){
+//     let random=Math.floor(Math.random()*qoutes.length)
+//     qoute.innerText= qoutes[random].Qoute
+//     person.innerText= qoutes[random].Person
     
-})
+// })
